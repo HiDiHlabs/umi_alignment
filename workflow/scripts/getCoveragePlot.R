d7de880f81867a3b22036b4385c429f4d94ab049
Sahay, Shashwat <shashwat.sahay@charite.de>
2024-02-08 18:17:07 +0100

    Added support for coverage plot creation

--- workflow/scripts/getCoveragePlot.R
+++ workflow/scripts/getCoveragePlot.R
@@ -8,9 +8,9 @@ option_list <- list(
     make_option(
         c("-o", "--output"), type = "character", default = NULL,
         help = "Output png"),
-    make_option(
-        c("-r", "--rds"), type = "character", default = NULL,
-        help = "Output rds"),
+    # make_option(
+    #     c("-r", "--rds"), type = "character", default = NULL,
+    #     help = "Output rds"),
     make_option(
         c("-b", "--binSize"), type = "integer", default = 50,
         help = "BinSize")
@@ -26,12 +26,12 @@ if (is.na(opt$input)){
 if (is.na(opt$output)) {
     stop("Output must be provided")
 }
-print(opt$input)
+# print(opt$input)
 
-print(opt$output)
-print(opt$binSize)
+# print(opt$output)
+# print(opt$binSize)
 
-print(opt$rds)
+# print(opt$rds)
 
 library(ACE)
 library(QDNAseq)
@@ -41,7 +41,7 @@ future::plan("multisession", workers=20)
 bins <-getBinAnnotations(binSize=opt$binSize)
 readCounts <- binReadCounts(bins, bamfiles=opt$input, chunkSize=10e6)
 readCountsFiltered <- applyFilters(readCounts, residual=TRUE, blacklist=TRUE)
-saveRDS(readCounts, file=opt$rds)
+# saveRDS(readCounts, file=opt$rds)
 readCountsFiltered <- estimateCorrection(readCountsFiltered)
 png(filename=opt$output, width=3000, height=1000, res=300)
 # par(mar=c(1,1,1,1))
